f6009403c249ce3cec3f2722a9801d99482c292b
Candy Tsai <candy02058912@gmail.com>
2019-12-23 13:31:52 +0800

    update for deployment

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+  apiServerUrl: 'https://agile-hamlet-83231.herokuapp.com', // the running FLASK api server url
+  auth0: {
+    url: 'riji', // the auth0 domain prefix
+    audience: 'dev', // the audience set for the auth0 app
+    clientId: '71DKzlfPKcAoVNsrcIyTyIj11z4flHnY', // the client id generated for the auth0 app
+    callbackURL: 'https://this-cafe.netlify.com' // the base url of the running ionic application.
+  }
 };
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -4,11 +4,11 @@
 
 export const environment = {
   production: false,
-  apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
+  apiServerUrl: 'https://agile-hamlet-83231.herokuapp.com', // the running FLASK api server url
   auth0: {
     url: 'riji', // the auth0 domain prefix
     audience: 'dev', // the audience set for the auth0 app
     clientId: '71DKzlfPKcAoVNsrcIyTyIj11z4flHnY', // the client id generated for the auth0 app
-    callbackURL: 'http://localhost:4200' // the base url of the running ionic application.
+    callbackURL: 'https://this-cafe.netlify.com' // the base url of the running ionic application.
   }
 };
